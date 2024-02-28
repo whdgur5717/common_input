0717576c69b7f482c253e1a54cd2e634a54fd42f
이종혁 <whdgur57117@gmail.com>
2024-02-29 02:33:59 +0900

    CommonInput 컴포넌트 생성

---
+++ src/Input/CommonInput.tsx
@@ -0,0 +1,32 @@
+import { ChangeEvent, ReactNode, useState } from "react"
+
+interface SearchInputProps {
+  value?: string
+  onChange?: (value: string) => void
+  children?: ReactNode
+}
+
+const SearchInput = ({ value: outerStateValue, onChange }: SearchInputProps) => {
+  const isControlled = outerStateValue !== undefined
+
+  const [innerStateValue, setInnerStateValue] = useState(outerStateValue)
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target
+    if (!isControlled) {
+      setInnerStateValue(value)
+    }
+    onChange?.(value) // onChange가 있을 경우에만 실행
+  }
+
+  return (
+    <>
+      <input
+        value={isControlled ? outerStateValue : innerStateValue}
+        onChange={handleChange}
+      />
+    </>
+  )
+}
+
+export default SearchInput
